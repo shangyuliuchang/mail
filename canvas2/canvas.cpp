cb3e9fbdbbe2085da31ad25cb62849dac25673fd
shangyuliuchang@163.com <shangyuliuchang@163.com>
2021-11-21 13:24:20 +0800

    calendar, announcement, filenames

--- canvas2/canvas.cpp
+++ canvas2/canvas.cpp
@@ -40,8 +40,10 @@ struct info
     string name;
     string title;
     string url;
-    string due;
+    string date;
+    string timeStr;
     bool read;
+    bool submitted;
     tm time;
 };
 enum valueType
@@ -80,7 +82,11 @@ void decodeMessage(vector<info> &list, const vector<values> &keys)
             {
                 end = recvBody.find(",\"", pos);
                 if (end == string::npos)
-                    return;
+                {
+                    end = recvBody.find("}", pos);
+                    if (end == string::npos)
+                        return;
+                }
                 tmpStr = recvBody.substr(pos, end - pos);
                 if (tmpStr.data()[0] == '\"' && tmpStr.data()[tmpStr.length() - 1] == '\"')
                     tmpStr = tmpStr.substr(1, tmpStr.length() - 2);
@@ -88,16 +94,19 @@ void decodeMessage(vector<info> &list, const vector<values> &keys)
                     newInfo.name = tmpStr;
                 if (keys[i].key.find("title") != string::npos)
                     newInfo.title = tmpStr;
+                if (keys[i].key.find("date") != string::npos)
+                    newInfo.date = tmpStr;
                 if (keys[i].key.find("due") != string::npos)
                 {
-                    newInfo.due = tmpStr;
+                    newInfo.date = tmpStr;
                     sscanf(tmpStr.data(), "%d-%d-%dT%d:%d:%dZ", &(tmpTm.tm_year), &(tmpTm.tm_mon), &(tmpTm.tm_mday), &(tmpTm.tm_hour), &(tmpTm.tm_min), &(tmpTm.tm_sec));
                     tmpTm.tm_year -= 1900;
                     tmpTm.tm_mon--;
                     tmpTime = mktime(&tmpTm);
                     tmpTime += 60 * 60 * 8;
-                    tmpTm = *gmtime(&tmpTime);
+                    tmpTm = *localtime(&tmpTime);
                     newInfo.time = tmpTm;
+                    newInfo.timeStr = asctime(&tmpTm);
                 }
                 if (keys[i].key.find("url") != string::npos)
                     newInfo.url = tmpStr;
@@ -106,6 +115,11 @@ void decodeMessage(vector<info> &list, const vector<values> &keys)
                         newInfo.read = true;
                     else
                         newInfo.read = false;
+                if (keys[i].key.find("submitted") != string::npos)
+                    if (tmpStr == "true")
+                        newInfo.submitted = true;
+                    else
+                        newInfo.submitted = false;
             }
             else if (keys[i].valType == num)
             {
@@ -121,14 +135,14 @@ void getCurrentTime(string &nowOut, string &nextMonOut)
 {
     using namespace std::chrono;
     time_t now = system_clock::to_time_t(system_clock::now());
-    time_t nextMon = now + 3600 * 24 * 30;
     struct tm *nowInfo = localtime(&now);
-    struct tm *nextMonInfo = localtime(&nextMon);
-    char nowStr[100], nextMonStr[100];
+    char nowStr[100];
     strftime(nowStr, 100, "%F", nowInfo);
-    strftime(nextMonStr, 100, "%F", nextMonInfo);
     nowOut = nowStr;
-    nextMonOut = nextMonStr;
+    now += 3600 * 24 * 30;
+    nowInfo = localtime(&now);
+    strftime(nowStr, 100, "%F", nowInfo);
+    nextMonOut = nowStr;
 }
 void getAllAnns(const vector<info> &courses, vector<vector<info>> &anns)
 {
@@ -148,16 +162,149 @@ void getAllAnns(const vector<info> &courses, vector<vector<info>> &anns)
         anns.push_back(annInfo);
     }
 }
-int main(void)
+void getCourses(vector<info> &courseInfo)
 {
     string courseURL = "https://umjicanvas.com/api/v1/users/self/favorites/courses?per_page=50" + token;
     vector<values> courseKeys({{"id", num}, {"name", str}});
-    vector<info> courseInfo;
-    vector<vector<info>> anns;
-    curl_global_init(CURL_GLOBAL_ALL);
     getMessage(courseURL);
     decodeMessage(courseInfo, courseKeys);
+}
+void getCalendar(const vector<info> &course, vector<info> &calendar)
+{
+    stringstream ss;
+    string now, nextMon;
+    ss.clear();
+    ss.str("");
+    ss << "https://umjicanvas.com/api/v1/calendar_events?type=assignment&per_page=50";
+    for (size_t i = 0; i < course.size(); i++)
+        ss << "&context_codes[]=course_" << course[i].id;
+    getCurrentTime(now, nextMon);
+    ss << "&start_date=" << now;
+    ss << "&end_date=" << nextMon;
+    ss << token;
+    vector<values> calendarKeys({{"title", str}, {"due_at", str}, {"user_submitted", str}});
+    getMessage(ss.str());
+    decodeMessage(calendar, calendarKeys);
+}
+void getFiles(const vector<info> &course, vector<vector<info>> &files)
+{
+    files.clear();
+    vector<info> singleCourseFile;
+    stringstream ss;
+    vector<values> fileKeys({{"id", num}, {"display_name", str}, {"updated_at", str}});
+    for (size_t i = 0; i < course.size(); i++)
+    {
+        ss.clear();
+        ss.str("");
+        ss << "https://umjicanvas.com/api/v1/courses/" << course[i].id << "/files?per_page=50&sort=updated_at&order=desc" << token;
+        getMessage(ss.str());
+        decodeMessage(singleCourseFile, fileKeys);
+        files.push_back(singleCourseFile);
+    }
+}
+void storeFileNames(const vector<info> &course, const vector<vector<info>> &files)
+{
+    ofstream f;
+    stringstream ss;
+    if (course.size() != files.size())
+        return;
+    for (size_t i = 0; i < course.size(); i++)
+    {
+        ss.str("");
+        ss.clear();
+        ss << "filenames/" << course[i].id << ".txt";
+        f.open(ss.str(), ios::out);
+        for (size_t j = 0; j < files[i].size(); j++)
+        {
+            f << files[i][j].id << " " << files[i][j].date << " " << endl;
+        }
+        f.close();
+    }
+}
+void loadFileNames(const vector<info> &course, vector<vector<info>> &files)
+{
+    stringstream ss;
+    ifstream f;
+    string tmp;
+    vector<info> singleCourseFiles;
+    info newFile;
+    char date[100];
+    int id;
+    files.clear();
+    for (size_t i = 0; i < course.size(); i++)
+    {
+        ss.clear();
+        ss.str("");
+        ss << "filenames/" << course[i].id << ".txt";
+        singleCourseFiles.clear();
+        f.open(ss.str(), ios::in);
+        while (f.is_open() && !f.eof())
+        {
+            getline(f, tmp);
+            if (tmp.empty())
+                break;
+            sscanf(tmp.data(), "%d %s", &id, date);
+            newFile.id = id;
+            newFile.date = date;
+            singleCourseFiles.push_back(newFile);
+        }
+        f.close();
+        files.push_back(singleCourseFiles);
+    }
+}
+void removeOldFiles(vector<vector<info>> &filenames, const vector<vector<info>> &oriFilenames)
+{
+    size_t indexCurr, indexOri;
+    for (size_t i = 0; i < filenames.size(); i++)
+    {
+        indexCurr = 0;
+        indexOri = 0;
+        while (indexCurr < filenames[i].size() && indexOri < oriFilenames[i].size())
+        {
+            if (filenames[i][indexCurr].id == oriFilenames[i][indexOri].id && filenames[i][indexCurr].date == oriFilenames[i][indexOri].date)
+                break;
+            if (filenames[i][indexCurr].date > oriFilenames[i][indexOri].date)
+                indexCurr++;
+            else
+                indexOri++;
+        }
+        filenames[i] = vector<info>(filenames[i].begin(), filenames[i].begin() + indexCurr);
+    }
+}
+void getPublicURL(vector<vector<info>> &files)
+{
+    stringstream ss;
+    vector<values> urlKeys({{"public_url", str}});
+    vector<info> publicURLs;
+    for (size_t i = 0; i < files.size(); i++)
+    {
+        for (size_t j = 0; j < files[i].size(); j++)
+        {
+            ss.clear();
+            ss.str("");
+            ss << "https://www.umjicanvas.com/api/v1/files/" << files[i][j].id << "/public_url?" << token;
+            getMessage(ss.str());
+            decodeMessage(publicURLs, urlKeys);
+            files[i][j].url = publicURLs[0].url;
+        }
+    }
+}
+int main(void)
+{
+    vector<info> courseInfo;
+    vector<vector<info>> anns;
+    vector<info> calendarInfo;
+    vector<vector<info>> files;
+    vector<vector<info>> originalFiles;
+    curl_global_init(CURL_GLOBAL_ALL);
+    getCourses(courseInfo);
     getAllAnns(courseInfo, anns);
+    getCalendar(courseInfo, calendarInfo);
+    getFiles(courseInfo, files);
+    loadFileNames(courseInfo, originalFiles);
+    // storeFileNames(courseInfo, files);
+    removeOldFiles(files, originalFiles);
+    getPublicURL(files);
     curl_global_cleanup();
     return 0;
 }
